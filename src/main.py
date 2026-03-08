eee585a944facde1002faa244019a74c01db6ec9
Terry Luan <terryluan12@gmail.com>
2026-03-08 01:40:17 -0500

    Changed folder structure and formatted all files

--- src/main.py
+++ src/main.py
@@ -9,9 +9,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 def home_page(request: Request):
     return templates.TemplateResponse(request=request, name="landing.html")
+
 
 @app.get("/about", response_class=HTMLResponse)
 def about_us_page(request: Request):
@@ -22,11 +24,12 @@ def about_us_page(request: Request):
 def get_involved_page(request: Request):
     return templates.TemplateResponse(request=request, name="get-involved.html")
 
+
 @app.get("/content", response_class=HTMLResponse)
 def content_page(request: Request):
     return templates.TemplateResponse(request=request, name="content.html")
 
+
 @app.get("/volunteer", response_class=HTMLResponse)
 def volunteer_form_page(request: Request):
     return templates.TemplateResponse(request=request, name="about-us.html")
-
