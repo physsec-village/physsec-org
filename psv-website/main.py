c4f86736e2e88a4475ad17830cf04f7abe2909ab
Terry Luan <terryluan12@gmail.com>
2026-03-07 21:47:25 -0500

    Added pages * added the "About Us", "Get Involved", and "Content" pages

--- psv-website/main.py
+++ psv-website/main.py
@@ -10,5 +10,23 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+def home_page(request: Request):
     return templates.TemplateResponse(request=request, name="landing.html")
+
+@app.get("/about", response_class=HTMLResponse)
+def about_us_page(request: Request):
+    return templates.TemplateResponse(request=request, name="about-us.html")
+
+
+@app.get("/involved", response_class=HTMLResponse)
+def get_involved_page(request: Request):
+    return templates.TemplateResponse(request=request, name="get-involved.html")
+
+@app.get("/content", response_class=HTMLResponse)
+def content_page(request: Request):
+    return templates.TemplateResponse(request=request, name="content.html")
+
+@app.get("/volunteer", response_class=HTMLResponse)
+def volunteer_form_page(request: Request):
+    return templates.TemplateResponse(request=request, name="about-us.html")
+
