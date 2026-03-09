b9c0fa980a287ab052fb9863f870e980d2415245
Terry Luan <terryluan12@gmail.com>
2026-03-09 00:29:58 -0400

    * Added a calls page * Separated styles into different files

--- src/forms/router.py
+++ src/forms/router.py
@@ -3,10 +3,14 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 
 from ..dependencies import templates
 
-
 router = APIRouter(prefix="/forms")
 
 
 @router.get("/volunteer", response_class=HTMLResponse)
 def volunteer_form_page(request: Request):
     return templates.TemplateResponse(request=request, name="pages/volunteer-form.html")
+
+
+@router.get("/calls", response_class=HTMLResponse)
+def calls_page(request: Request):
+    return templates.TemplateResponse(request=request, name="pages/calls.html")
